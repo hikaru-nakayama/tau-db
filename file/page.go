f5dfdba988d8b0a23788dd5fd9a4a16bd6c7b8ea
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-28 00:57:15 +0900

    Implement Get/SetInt method

--- file/page.go
+++ file/page.go
@@ -2,6 +2,7 @@ package file
 
 import (
 	"bytes"
+	"encoding/binary"
 )
 
 type Page struct {
@@ -11,3 +12,22 @@ type Page struct {
 func NewPage(size int) *bytes.Buffer {
 	return bytes.NewBuffer(make([]byte, size))
 }
+
+func NewPageFromByte(b []byte) *bytes.Buffer {
+	return bytes.NewBuffer(b)
+}
+
+func (p *Page) GetInt(offset int) int {
+	// The int type is 32 bit, and the Bigint type is 64 bit.
+	data := p.bb.Bytes()[offset : offset+4]
+	value := binary.BigEndian.Uint32(data)
+	return int(value)
+}
+
+func (p *Page) SetInt(offset int, n int) {
+
+	data := make([]byte, 4)
+	binary.BigEndian.PutUint32(data, uint32(n))
+	copy(p.bb.Bytes()[offset:], data)
+}
+
