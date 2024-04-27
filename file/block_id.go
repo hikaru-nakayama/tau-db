2eb2a74e1e0345a6949ef2cc10924fb463dbf42c
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-27 22:49:50 +0900

    add test for block_id

--- file/block_id.go
+++ file/block_id.go
@@ -1,5 +1,9 @@
 package file
 
+import (
+	"fmt"
+)
+
 type BlockId struct {
 	filename string
 	blknum   int
@@ -17,4 +21,10 @@ func (b *BlockId) Number() int {
 	return b.blknum
 }
 
+func (b *BlockId) Equals(other *BlockId) bool {
+	return b.filename == other.Filename() && b.blknum == other.Number()
+}
 
+func (b *BlockId) String() string {
+	return fmt.Sprintf("[file %s, block %d]", b.filename, b.blknum)
+}
---
+++ file/block_id_test.go
@@ -0,0 +1,25 @@
+package file
+
+import "testing"
+
+func TestEqual(t *testing.T) {
+	b1 := BlockId{filename: "test", blknum: 1}
+
+	tests := []struct {
+		filename string
+		num int
+		expected bool
+	}{
+		{"test", 1, true},
+		{"dumy", 1, false},
+		{"test", 2, false},
+		{"dumy", 2, false},
+	}
+
+	for _, tt := range tests {
+		b2 := BlockId{filename: tt.filename, blknum: tt.num}
+		if b1.Equals(&b2) != tt.expected {
+			t.Errorf("Equals return value is wrong")
+		}
+	}
+}
