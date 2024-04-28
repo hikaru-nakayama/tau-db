4560b258d41b0ae531fe1d738c7c23ae5ea80b6a
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-28 20:08:42 +0900

    update NewFileMgr test

--- file/file_mgr_test.go
+++ file/file_mgr_test.go
@@ -1,12 +1,13 @@
 package file
 
 import (
-	"testing"
 	"os"
+	"testing"
 )
 
 func TestNewFileMgr(t *testing.T) {
 	dir_name := "test_db"
+	
 	mgr := NewFileMgr(dir_name, 100)
 	_, err := os.Stat(dir_name)
 	if os.IsNotExist(err) {
@@ -14,6 +15,15 @@ func TestNewFileMgr(t *testing.T) {
 	}
 	if mgr.dbDirectory != dir_name {
 		t.Errorf("expected: %s, but got: %s", dir_name, mgr.dbDirectory)
+	}
+        f, err := os.Create("./test_db/tmp.txt")
+	if err != nil {
+		panic("fail to create tmp file for test")
+	}
+	NewFileMgr(dir_name ,100)
+	_, err = os.Stat(f.Name())
+	if !os.IsNotExist(err) {
+		t.Errorf("tmp file not deleted")
 	}
 	os.Remove(dir_name)
 }
