9d38d2185f881b4eee23d807930d4570bc26e015
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-05-19 20:25:59 +0900

    Implemet constructor for SetStringRecord

--- tx/set_string_record.go
+++ tx/set_string_record.go
@@ -11,3 +11,24 @@ type SetStringRecord struct {
 	val    string
 	blk    *file.BlockId
 }
+
+func NewSetStringRecord(p *file.Page) *SetStringRecord {
+	tpos := 4
+	txnum := p.GetInt(tpos)
+	fpos := tpos + 4
+	filename := p.GetString(fpos)
+	bpos := fpos + p.MaxLength(len(filename))
+	blknum := p.GetInt(bpos)
+	blk := file.NewBlockId(filename, blknum)
+	opos := bpos + 4
+	offset := p.GetInt(opos)
+	vpos := opos + 4
+	val := p.GetString(vpos)
+
+	return &SetStringRecord{
+		txnum:  txnum,
+		offset: offset,
+		val:    val,
+		blk:    blk,
+	}
+}
