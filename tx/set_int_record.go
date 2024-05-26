3b3b2eea9a6abc6aa266950fb13a009272d594dc
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-05-26 18:01:37 +0900

    fix SetIntRecord

--- tx/set_int_record.go
+++ tx/set_int_record.go
@@ -27,13 +27,12 @@ func NewSetIntRecord(p *file.Page) *SetIntRecord {
 	val := p.GetInt(vpos)
 
 	return &SetIntRecord{
-		txnum: txnum,
+		txnum:  txnum,
 		offset: offset,
-		val: val,
-		blk: blk
+		val:    val,
+		blk:    blk,
 	}
 }
-
 
 func (sir *SetIntRecord) Op() int {
 	return SETINT
@@ -49,12 +48,12 @@ func (sir *SetIntRecord) Undo(tx *Transaction) {
 	tx.UnPin(sir.blk)
 }
 
-func SetIntRecordWriteToLog(lm *log.LogMgr, txnum int, blk *file.BlockId, offset int, val string) (int error) {
+func SetIntRecordWriteToLog(lm *log.LogMgr, txnum int, blk *file.BlockId, offset int, val int) (int, error) {
 	tpos := 4
 	fpos := tpos + 4
 	bpos := fpos + file.MaxLength(len(blk.Filename()))
 	opos := bpos + 4
-	vops := opos + 4
+	vpos := opos + 4
 	reclen := vpos + 4
 	rec := make([]byte, reclen)
 	p := file.NewPageFromByte(rec)
@@ -66,5 +65,3 @@ func SetIntRecordWriteToLog(lm *log.LogMgr, txnum int, blk *file.BlockId, offset
 	p.SetInt(vpos, val)
 	return lm.Append(rec)
 }
-
-
