6bcb14e9829b85a063a70ceaa7d7da9bd2883eb6
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-05-26 16:52:02 +0900

    Implement Transaction interface

--- tx/transaction.go
+++ tx/transaction.go
@@ -1,4 +1,12 @@
 package tx
 
+import "github.com/hikaru-nakayama/tau-db.git/file"
+
+type ITransaction interface {
+	Pin(blk *file.BlockId)
+	SetString(blk *file.BlockId, offset int, val int, log bool)
+	UnPin(blk *file.BlockId)
+}
+
 type Transaction struct {
 }
