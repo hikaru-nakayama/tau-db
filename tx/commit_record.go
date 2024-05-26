ed99a2f2425c9188ea244b0e54533cc9356481dd
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-05-26 18:41:31 +0900

    Implement CommitRecord

---
+++ tx/commit_record.go
@@ -0,0 +1,37 @@
+package tx
+
+import (
+	"github.com/hikaru-nakayama/tau-db.git/file"
+	"github.com/hikaru-nakayama/tau-db.git/log"
+)
+
+type CommitRecord struct {
+	LogRecord
+	txnum int
+}
+
+func NewCommitRecord(p *file.Page) *CommitRecord {
+	tpos := 4
+	txnum := p.GetInt(tpos)
+	return &CommitRecord{
+		txnum: txnum,
+	}
+}
+
+func (cr *CommitRecord) Op() int {
+	return COMMIT
+}
+
+func (cr *CommitRecord) TxNumber() int {
+	return cr.txnum
+}
+
+func (cr *CommitRecord) Undo() {}
+
+func CommitRecordWriteToLog(lm *log.LogMgr, txnum int) (int, error) {
+	rec := make([]byte, 2*4)
+	p := file.NewPageFromByte(rec)
+	p.SetInt(0, COMMIT)
+	p.SetInt(4, txnum)
+	return lm.Append(rec)
+}
