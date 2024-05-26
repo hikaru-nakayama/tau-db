b6fb5d87424b1f75b68cf5e5874f16d437a01eb0
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-05-26 19:26:16 +0900

    Implement CheckPoint Record

---
+++ tx/checkpoint_record.go
@@ -0,0 +1,29 @@
+package tx
+
+import (
+	"github.com/hikaru-nakayama/tau-db.git/file"
+	"github.com/hikaru-nakayama/tau-db.git/log"
+)
+
+type CheckPointRecord struct {
+	LogRecord
+}
+
+func NewCheckPointRecord(p *file.Page) *CheckPointRecord {
+	return &CheckPointRecord{}
+}
+
+func (cr *CheckPointRecord) Op() int {
+	return CHECKPOINT
+}
+
+func (cr *CheckPointRecord) TxNumber() {}
+
+func (cr *CheckPointRecord) Undo() {}
+
+func CheckPointRecordWriteToLog(lm *log.LogMgr, txnum int) (int, error) {
+	rec := make([]byte, 4)
+	p := file.NewPageFromByte(rec)
+	p.SetInt(0, CHECKPOINT)
+	return lm.Append(rec)
+}
